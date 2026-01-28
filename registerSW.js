86b94fe9ae1586cd6326077d6c65986440f43a22
Waxmard <Waxmard@users.noreply.github.com>
2026-01-28 16:55:41 +0000

    deploy: 0d1b74c50a689feca7f8ca4dd2a9dbd5df57731b

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/pokemon-team-status/sw.js', { scope: '/pokemon-team-status/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
